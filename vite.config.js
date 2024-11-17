6a789b38b7b97794f9da1232974ee91dd1fb863c
Dafi <85431653+dafinrmaulana@users.noreply.github.com>
2024-11-18 01:59:10 +0700

    chore: change build out dir

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+   build: {
+    outDir: '../',
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
